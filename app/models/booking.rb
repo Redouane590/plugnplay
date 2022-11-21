a0edde30d796646740a53f28c4c61475e2557020
Redouane Elmahi <redouaneelmahi@gmail.com>
2022-11-21 17:32:41 +0100

    add validate status in booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,5 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   # validates :start_date, comparison: { less_than_or_equal_to: :end_date }
+  validates :status, presence: true
 end
