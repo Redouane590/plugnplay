ac907dc4f89cd189638cb4ba344ee6e454f5f1f1
Emma ROSSO <emmarosso.pro@gmail.com>
2022-11-23 16:28:06 +0100

    cloudinary + photo upload

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -50,6 +50,6 @@ class BoatsController < ApplicationController
   private
 
   def boat_params
-    params.require(:boat).permit(:title, :description, :photo_url, :price, :category, :address)
+    params.require(:boat).permit(:title, :description, :photo_url, :price, :category, :address, :photo)
   end
 end
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,10 +1,10 @@
 class Boat < ApplicationRecord
-  CATEGORY = ["Bateau à voile", "Bateau electrique", "Bateau pirate", "Bateau de luxe"]
+  CATEGORY = ["Voilier", "Catamaran", "Bateau electrique", "Bateau pirate", "Bateau de luxe"]
 
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
   validates :description, presence: true
-  validates :photo_url, presence: true
   validates :price, presence: true, numericality: true
   validates :address, presence: true, length: { minimum: 4 }
   validates :category, presence: true
