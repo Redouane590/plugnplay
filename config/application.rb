47b973bf08844506ccec2efe0351bb1ede901922
Redouane Elmahi <redouaneelmahi@gmail.com>
2022-11-24 18:02:16 +0100

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Plugnplay
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
